6d5bf4cd5b84a703f50425d4779c1d12b1202a27
Zane Hitchcox <zwhitchcox@gmail.com>
2019-10-16 14:33:49 -0400

    wip

--- lib.ts
+++ lib.ts
@@ -1,5 +1,5 @@
 import { fieldPresets } from './field-presets';
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import memoize from "lodash/memoize"
 
 const getHumanList = (arr, conjunction = "and") => [arr.slice(0, -1).join(', '), arr.slice(-1)[0]]
@@ -26,27 +26,43 @@ const getProp = (prop: string | {[key: string]: any}) => {
   }
   return prop
 }
-const memoizedGetProp = memoize(getProp)
 
-const getTouchedDefault = properties => Object.keys(properties)
+const getTouchedInit = properties => Object.keys(properties)
   .reduce((acc, cur) => (acc[cur] = false, acc), {})
-const memoizedGetTouchedDefault = memoize(getTouchedDefault)
 
-export const fieldz = properties => {
+export const fieldz = fields => {
   const [errors, setErrors] = useState([])
-  const [touched, setTouched] = useState(memoizedGetTouchedDefault(properties))
-  for (const key in properties) {
-    const {validate, preset, ...fields}= memoizedGetProp(properties[key])
-    const fieldProps = {
+  const initTouched = useMemo(() => getTouchedInit(fields), [fields])
+  const [touched, setTouched] = useState(initTouched)
+
+  const result = {}
+  for (const fieldName in fields) {
+    const {validate, preset, ...additionalProps}= useMemo(() => getProp(fields[fieldName]), [fields[fieldName]])
+    const props = useMemo(() => ({
       ...fields,
       onChange: e => {
-        try {
-          validate(e.target.value)
-        } catch (e) {
-
+        if (!touched[fieldName]) {
+          setTouched({
+            ...touched,
+            fieldName: true,
+          })
         }
-      }
+        const fieldErrors = validate(e.target.value, {
+          collectAll: true,
+        })
+        setErrors({
+          ...errors,
+          [fieldName]: fieldErrors,
+        })
+      },
+      ...additionalProps
+    }), [validate, additionalProps])
+    result[fieldName] = {
+      props,
+      errors,
+      touched,
     }
   }
+  return result
 }
 
